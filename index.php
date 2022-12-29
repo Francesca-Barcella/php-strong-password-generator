2aff4842fc45aacdcc753e0c53e924aa6836c2f7
Francesca-Barcella <pollon.fra@gmail>
2022-12-29 19:32:18 +0100

    esportate funzioni e collegate con include

--- index.php
+++ index.php
@@ -21,44 +21,7 @@ Invece di visualizzare la password nella index, effettuare un redirect ad una pa
 
 //var_dump($_GET);
 
-//funzione per generare la pwd
-function generate_password($pwd_len)
-{
-    //lugnhezza pwd compresa tra 8 e 32
-
-    if ($pwd_len < 8 || $pwd_len > 32) {
-        //var_dump('La password deve essere compresa tra 8 e 32 caratteri');
-        //return 'Errore! La password deve essere compresa tra 8 e 32 caratteri.';
-        return ['class' => 'danger', 'result' => 'Errore! La password deve essere compresa tra 8 e 32 caratteri.'];
-    }
-
-    //variabile vuota per creare la pwd
-    $password = '';
-
-    $characters_list = generate_characters($pwd_len);
-    //var_dump($characters_list);
-
-    //ciclo fino a raggiungere la lunghezza della pwd_len
-
-    while (strlen($password) < $pwd_len) {
-        //ad ogni iterazione sceglie un carattere della lista
-        $character = $characters_list[rand(0, strlen($characters_list))];
-        //var_dump($character);
-        //concateno il carattere scelto alla stringa della password
-        $password .= $character;
-    }
-    return ['class' => 'info', 'result' => $password];
-}
-
-//genero lista completa di caratteri - numeri  - simboli e poi li metto in una funzione a sestante rispetto a quella sopra
-function generate_characters()
-{
-    $letters = 'abcdefghilmnopqrstuvz';
-    $letter_uppercase = strtoupper($letters);
-    $numbers = '1234567890';
-    $symbols = '#+-_?![]{}%&$*<>=';
-    return $letters . $letter_uppercase . $numbers . $symbols;
-}
+include __DIR__ . '/functions.php';
 
 
 if (isset($_GET['pwdLen'])) {
